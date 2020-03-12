37116882dc93f5e0e5d745f4dd68fbb30172e6e7
dmist1 <61623821+dmist1@users.noreply.github.com>
2020-03-12 20:19:15 +0300

    FINAL

--- projekt1/js/script.js
+++ projekt1/js/script.js
@@ -13,14 +13,20 @@ $(document).ready(function() {
 				breakpoint:700,
 				settings:{
 					slidesToShow:1,
+					arrows:false,
 				}
 			}
 		],
 		draggable:false,
 	});
 
-	$('.header-burger').click(function(event) {
+	$('.header-burger').click(function() {
 		$('.header-burger,.header-menu,.header-menu-list,.header-menu-item').toggleClass('active');
+		$('body').toggleClass('lock');
+	});
+	$('.header-menu-item').click(function() {
+		$('.header-burger,.header-menu,.header-menu-list,.header-menu-item').removeClass('active');
+		$('body').removeClass('lock');
 	});
 
 });
